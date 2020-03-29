f68deec88166cad6785c0028e70db4bf0657b339
cdk8s-zelda <160668873@qq.com>
2020-03-29 13:15:42 +0800

    docs: user attribute ext

--- tkey-sso-client-starter-rest/src/main/java/com/cdk8s/tkey/client/rest/pojo/dto/OauthUserAttribute.java
+++ tkey-sso-client-starter-rest/src/main/java/com/cdk8s/tkey/client/rest/pojo/dto/OauthUserAttribute.java
@@ -6,6 +6,7 @@ import lombok.Setter;
 import lombok.ToString;
 
 import java.io.Serializable;
+import java.util.Map;
 
 @Getter
 @Setter
@@ -15,8 +16,13 @@ public class OauthUserAttribute implements Serializable {
 
 	private static final long serialVersionUID = -5890673731362415002L;
 
-	private String email;
 	private String userId;
 	private String username;
+	private String nickname;
+	private String realName;
+	private String avatarUrl;
+	private String email;
+	private String mobilePhone;
+	private Map<String, Object> userAttributeExt;
 
 }
